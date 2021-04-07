a33b50a560bb8c54326b3ba991bd1633fa7923be
asmaa <asmaaesmail0@gmail.com>
2021-04-07 15:34:21 +0200

    front

---
+++ js/button.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    var btn1 = document.querySelector('.material-btn');
+btn1.onmouseover= function(event){
+  var wave = btn1.querySelector('.wave');
+  var className = 'material-btn--active';
+  if(btn1.classList.contains(className)){
+    btn1.classList.remove(className);
+  }
+  wave.style.left = event.clientX - btn1.offsetLeft +'px';
+  wave.style.top = event.clientY - btn1.offsetTop + 'px'; 
+  
+  void btn1.offsetWidth;
+  btn1.classList.add(className);
+}
+
+
+});
